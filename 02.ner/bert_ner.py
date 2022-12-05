5f64de69f742f6b8dff0693a15ccaec13e732ad5
johnsondzd <johnsondzd@qq.com>
2022-12-05 17:51:41 +0800

    模型对验证数据的验证。

--- 02.ner/bert_ner.py
+++ 02.ner/bert_ner.py
@@ -15,6 +15,11 @@ from transformers import BertTokenizer
 from transformers import BertModel
 from transformers import AdamW
 
+from seqeval.metrics import accuracy_score as seq_accuracy_score
+from seqeval.metrics import precision_score as seq_precision_score
+from seqeval.metrics import recall_score as seq_recall_score
+from seqeval.metrics import f1_score as seq_f1_score
+
 
 def read_data(file):
     with open(file, "r", encoding="utf-8") as f:
@@ -44,7 +49,7 @@ def build_label(train_label):
         for l in label:
             if l not in label_2_index:
                 label_2_index[l] = len(label_2_index)
-    return label_2_index
+    return label_2_index, list(label_2_index)
 
 
 class BertDataset(Dataset):
@@ -63,7 +68,7 @@ class BertDataset(Dataset):
         label_index = [0] + [self.label_2_index.get(l, 1) for l in label] + [0] + (self.max_len - len(text)) * [0]
 
         label_index = torch.tensor(label_index)
-        return text_index.reshape(-1), label_index
+        return text_index.reshape(-1), label_index, len(label)
 
     def __len__(self):
         return self.all_text.__len__()
@@ -94,11 +99,11 @@ if __name__ == '__main__':
     train_text, train_label = read_data(os.path.join('data', 'train.txt'))
     dev_text, dev_label = read_data(os.path.join('data', 'dev.txt'))
     test_text, test_label = read_data(os.path.join('data', 'test.txt'))
-    label_2_index = build_label(test_label)
+    label_2_index, index_2_label = build_label(test_label)
 
     tokenizer = BertTokenizer.from_pretrained(os.path.join('..', 'bert-base-chinese'))
 
-    batch_size = 128
+    batch_size = 64
     epoch = 100
     max_len = 30
     lr = 0.0001
@@ -108,11 +113,16 @@ if __name__ == '__main__':
     train_dataset = BertDataset(train_text, train_label, label_2_index, max_len, tokenizer)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 
+    dev_dataset = BertDataset(dev_text, dev_label, label_2_index, max_len, tokenizer)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=batch_size, shuffle=False)
+
     model = BertNERModel(len(label_2_index)).to(device)
     opt = AdamW(model.parameters(), lr)
 
     for e in range(epoch):
-        for batch_text_index, batch_label_index in train_dataloader:
+
+        model.train()
+        for batch_text_index, batch_label_index, batch_len in train_dataloader:
             batch_text_index = batch_label_index.to(device)
             batch_label_index = batch_label_index.to(device)
             loss = model.forward(batch_text_index, batch_label_index)
@@ -123,4 +133,32 @@ if __name__ == '__main__':
 
             print(f'epoch: {e}, loss: {loss:.4f}')
 
+        model.eval()
 
+        all_pre = []
+        all_tag = []
+        for batch_text_index, batch_label_index, batch_len in dev_dataloader:
+            if len(batch_text_index) == 0:
+                print(batch_text_index)
+            batch_text_index = batch_label_index.to(device)
+            batch_label_index = batch_label_index.to(device)
+            pre = model.forward(batch_text_index)
+
+            pre = pre.cpu().numpy().tolist()
+            tag = batch_label_index.cpu().numpy().tolist()
+
+            for p, t, l in zip(pre, tag, batch_len):
+                p = p[1: 1 + l]
+                t = t[1: 1 + l]
+
+                p = [index_2_label[i] for i in p]
+                t = [index_2_label[i] for i in t]
+
+                all_pre.append(p)
+                all_tag.append(t)
+            # pre = [[index_2_label[j] for j in i] for i in pre]
+            # batch_label_index = [[index_2_label[j] for j in i] for i in batch_label_index]
+
+        f1_score = seq_f1_score(all_tag, all_pre)
+
+        print(f"f1: {f1_score}")
