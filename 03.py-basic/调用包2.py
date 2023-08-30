2f94ec47045e34f614e812142f75c06f79bf59e9
johnsondiao <johnsondzd@qq.com>
2023-08-30 16:15:17 +0800

    主函数的调包示例。。

--- 03.py-basic/调用包2.py
+++ 03.py-basic/调用包2.py
@@ -1,2 +1,12 @@
+a = 10
+
+# from import_page.func1 import f
+
 a2 = 20
 b2 = 12
+
+
+if __name__ == '__main__':
+    print("hello 包2")
+    # f(100)
+    a3 = 200
--- import_page/func1.py
+++ import_page/func1.py
@@ -5,3 +5,8 @@ from 调用包2 import a2
 a = 10
 b = 20
 c = 30
+
+
+def f(a):
+    # from 调用包2 import a2
+    print(a+a2)
