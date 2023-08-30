f31abf764247c5c1fac1b5c8799728e9fda159dc
johnsondiao <johnsondzd@qq.com>
2023-08-30 15:50:12 +0800

    修改模型名称！！

--- 12.手写new bing/flask_api.py
+++ 12.手写new bing/flask_api.py
@@ -25,8 +25,8 @@ def answer():
 
 if __name__ == '__main__':
 
-    tokenizer = AutoTokenizer.from_pretrained(os.path.join('..', 'models', 'chatglm-6b.bak'), trust_remote_code=True)
-    model = AutoModel.from_pretrained(os.path.join('..', 'models', 'chatglm-6b.bak'), trust_remote_code=True).half().cuda()
+    tokenizer = AutoTokenizer.from_pretrained(os.path.join('..', 'models', 'chatglm-6b'), trust_remote_code=True)
+    model = AutoModel.from_pretrained(os.path.join('..', 'models', 'chatglm-6b'), trust_remote_code=True).half().cuda()
     model = model.eval()
 
     app.run(host='0.0.0.0', port=8088)
